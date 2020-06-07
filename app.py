010553b3d87a8be0139e5a19816dc7758e55028f
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-06-07 04:45:56 +0200

    added a modal

--- app.py
+++ app.py
@@ -65,7 +65,20 @@ def header():
                     value=spreadsheet_list[0],
                 ),
                 style={"width": "200px"},
-            )
+            ),
+            dbc.NavItem(dbc.Button('Halp', id='open')),
+            dbc.Modal(
+                [
+                    dbc.ModalHeader("Help"),
+                    dbc.ModalBody("This is the content of the modal"),
+                    dbc.ModalFooter(
+                        dbc.Button("Close", id="close", className="ml-auto")
+                    ),
+                ],
+                id="modal",
+                size="lg",
+                centered=True,
+            ),
         ],
         brand=app.title,
         brand_href="#",
@@ -122,6 +135,19 @@ app.layout = html.Div(
         ),
     ],
 )
+
+
+# Modal
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"),
+     Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
 
 
 # Tabs
