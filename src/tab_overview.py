66f00a57144f0d45ccb982b283b3f1484251f6cd
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-06-07 12:57:13 +0200

    responsive main tab

--- src/tab_overview.py
+++ src/tab_overview.py
@@ -15,8 +15,13 @@ def tab_overview():
                     html.Div(
                         id="description-card",
                         children=[
-                            html.H3(
-                                "General overview of the spreadsheet"),
+                            html.H3("General overview of the spreadsheet"),
+                            dbc.Card(
+                                """Drag to select on the top graphs, the table will update based on this selection. """
+                                """Clicking on the table headers will sort, while typing in the first row will search. """,
+                                body=True,
+                                id="intro"
+                            ),
                         ],
                     ),
                 ]
@@ -30,75 +35,105 @@ def tab_overview():
                     dbc.Col(
                         dbc.Card([
                             dbc.CardBody([
-                                html.H4("Card title",
-                                        id="cardText1",
-                                        className="card-title"),
-                                html.P("Number of albums",
-                                       className="card-text"),
+                                html.H4(
+                                    "Card title",
+                                    id="cardText1",
+                                    className="card-title"
+                                ),
+                                html.P(
+                                    "Number of albums", className="card-text"
+                                ),
                             ]),
                         ]),
                     ),
                     dbc.Col(
                         dbc.Card([
                             dbc.CardBody([
-                                html.H4("Card title",
-                                        id="cardText2",
-                                        className="card-title"),
-                                html.P("Number of artists",
-                                       className="card-text"),
+                                html.H4(
+                                    "Card title",
+                                    id="cardText2",
+                                    className="card-title"
+                                ),
+                                html.P(
+                                    "Number of artists", className="card-text"
+                                ),
                             ]),
                         ]),
                     ),
                     dbc.Col(
                         dbc.Card([
                             dbc.CardBody([
-                                html.H4("Card title",
-                                        id="cardText3",
-                                        className="card-title"),
-                                html.P("Vote average",
-                                       className="card-text"),
+                                html.H4(
+                                    "Card title",
+                                    id="cardText3",
+                                    className="card-title"
+                                ),
+                                html.P("Vote average", className="card-text"),
                             ]),
                         ]),
                     ),
                 ]),
             ),
         ),
-        dbc.Row([
-            dbc.Col(
-                dcc.Graph(id="overview_year",
-                          figure={'layout': graph_custom}),
-            ),
-            dbc.Col(
-                dcc.Graph(id="overview_stats",
-                          figure={'layout': graph_custom}),
-            ),
-        ], className="mb-4 mt-4",),
-        dbc.Row([
-            dbc.Col(
-                dt.DataTable(
-                    id='overview_year_tbl',
-                    columns=[
-                        {'id': "Year", 'name': 'Year'},
-                        {'id': "Artist", 'name': 'Artist'},
-                        {'id': "Album", 'name': 'Album'},
-                        {'id': "AVG", 'name': 'Average'},
-                        {'id': "Votes", 'name': 'No. Votes'},
-                    ],
-                    style_header={
-                        'backgroundColor': 'rgb(30, 30, 30)'
-                    },
-                    style_cell={
-                        'backgroundColor': 'rgb(50, 50, 50)',
-                        'color': 'white'
-                    },
-                    style_filter={
-                        'backgroundColor': 'rgb(185, 167, 167)',
-                        'color': 'white'
-                    },
-                    page_size=12,
-                    sort_action='native',
-                    filter_action='native',
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Graph(
+                        id="overview_year", figure={'layout': graph_custom}
+                    ),
+                    md=6,
                 ),
-            ),
-        ], className="mb-4 mt-4",)
+                dbc.Col(
+                    dcc.Graph(
+                        id="overview_stats", figure={'layout': graph_custom}
+                    ),
+                    md=6,
+                ),
+            ],
+            className="mb-4 mt-4",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dt.DataTable(
+                        id='overview_year_tbl',
+                        columns=[
+                            {
+                                'id': "Year",
+                                'name': 'Year'
+                            },
+                            {
+                                'id': "Artist",
+                                'name': 'Artist'
+                            },
+                            {
+                                'id': "Album",
+                                'name': 'Album'
+                            },
+                            {
+                                'id': "AVG",
+                                'name': 'Average'
+                            },
+                            {
+                                'id': "Votes",
+                                'name': 'No. Votes'
+                            },
+                        ],
+                        style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+                        style_cell={
+                            'backgroundColor': 'rgb(50, 50, 50)',
+                            'color': 'white'
+                        },
+                        style_filter={
+                            'backgroundColor': 'rgb(185, 167, 167)',
+                            'color': 'white'
+                        },
+                        page_size=12,
+                        sort_action='native',
+                        filter_action='native',
+                    ),
+                ),
+            ],
+            className="mb-4 mt-4",
+        )
     ]
