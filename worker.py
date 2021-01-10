cfacff1cfd2b7cb574fa9f2e9e289e1e3eac69ff
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-01-10 23:46:13 +0100

    again

--- worker.py
+++ worker.py
@@ -1,23 +1,36 @@
 import os
 import redis
+import aioredis
 import asyncio
 from src.discord_bot import disc_bot
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
-p = conn.pubsub()
-p.subscribe('discord')
-
 botty = disc_bot()
 
 
-async def listen_redis():
+# conn = redis.from_url(redis_url)
+# p = conn.pubsub()
+# p.subscribe('discord')
+async def reader(ch):
+    while (await ch.wait_message()):
+        msg = await ch.get()
+        await botty.print(msg['data'])
+
+
+async def main():
 
     await botty.print("Starting to listen.")
 
-    for new_message in p.listen():
-        await botty.print(new_message['data'])
+    conn = await aioredis.create_connection(redis_url)
+    res = await conn.subscribe('discord')
+    ch1 = res[0]
+    tsk = asyncio.ensure_future(reader(ch1))
+    await tsk
+
+    # gracefully closing underlying connection
+    conn.close()
+    await conn.wait_closed()
 
 
 if __name__ == '__main__':
-    asyncio.run(listen_redis())
+    asyncio.run(main())
